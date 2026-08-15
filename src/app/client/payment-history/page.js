311713b60805a0996b3320283637c623f660e7a2
virenderjangra28 <virender.jangra28@gmail.com>
2026-08-15 17:29:11 +0530

    sync with virender

--- src/app/client/payment-history/page.js
+++ src/app/client/payment-history/page.js
@@ -1,15 +1,5 @@
-<<<<<<< HEAD
-import React from 'react'
-
-export default function page() {
-  return (
-    <div>page</div>
-  )
-}
-=======
 import PaymentHistory from "./PaymentHistory";
 
 export default function PaymentHistoryPage() {
   return <PaymentHistory />;
 }
->>>>>>> virender
--- src/app/client/profile/page.js
+++ src/app/client/profile/page.js
@@ -1,17 +1,5 @@
-<<<<<<< HEAD
-"use client";
-
-import React from 'react'
-
-export default function page() {
-  return (
-    <div>page</div>
-  )
-}
-=======
 import ClientProfile from "./ClientProfile";
 
 export default function ClientProfilePage() {
   return <ClientProfile />;
 }
->>>>>>> virender

454b3a6bcbafe995b4c72b2f48a1c6ca3bf5062a
virenderjangra28 <virender.jangra28@gmail.com>
2026-08-19 12:49:41 +0530

    up

--- .next/dev/static/chunks/_219uq1s._.js
+++
@@ -1,5 +0,0 @@
-(globalThis["TURBOPACK_CHUNK_LISTS"] || (globalThis["TURBOPACK_CHUNK_LISTS"] = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: ["static/chunks/[root-of-the-server]__1qlqokq._.css","static/chunks/node_modules_03fecnd._.js","static/chunks/_1a7qevl._.js","static/chunks/src_sharedComponent_homeSection_05hkigw._.js"],
-    source: "entry"
-});
